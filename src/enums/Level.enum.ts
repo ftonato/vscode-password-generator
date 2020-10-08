893ae8b846fba6552dbcd53990cadcf8a7cb6bc4
Ademílson F. Tonato <ademilsonft@outlook.com>
2020-10-08 16:04:02 +0100

    feat: Summoning something into life (bringing ideas to life)

---
+++ src/enums/Level.enum.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+/**
+* Password strength level (ENUM)
+*/
+export enum Level {
+    WEAK = 'WEAK',
+    NORMAL = 'NORMAL',
+    STRONG = 'STRONG',
+}
